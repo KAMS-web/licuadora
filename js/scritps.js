fe2b522d041ae3ee0858231259e532222c3319a0
kams <kevinmora@gmail.com>
2025-04-03 11:14:59 -0600

    cambios de estructura para mas estetica

--- js/scritps.js
+++ js/scritps.js
@@ -1,29 +1,28 @@
 
 var estadoLicuadora = "apagado";
-var sonidoLicuadora = document.getElementById("blender-sound");
-var botonLicuadora = document.getElementById("blender-button-sound");
+var sonidoLicuadora = document.getElementById( "blender-sound" );
+var botonLicuadora = document.getElementById( "blender-button-sound" );
 var licuadora = document.getElementById("blender");
 
-function controlarLicuadora(){
-    if(estadoLicuadora == "apagado"){
-        estadoLicuadora = "encendido";
-        hacerBrrBrr();
-        licuadora.classList.add("active");
-    } else {
-        estadoLicuadora = "apagado";
-        hacerBrrBrr();
-        licuadora.classList.remove("active");
-
-    }
+function controlarLicuadora() {
+  if ( estadoLicuadora == "apagado" ) {
+    estadoLicuadora = "encendido";
+    hacerBrrBrr();
+    licuadora.classList.add("active");
+  } else {
+    estadoLicuadora = "apagado";
+    hacerBrrBrr();
+    licuadora.classList.remove("active");
+  }
 }
 
-function hacerBrrBrr(){
-    if( sonidoLicuadora.paused){
-        botonLicuadora.onplay();
-        sonidoLicuadora.play();
-    } else {
-        botonLicuadora.play();
-        sonidoLicuadora.pause();
-        sonidoLicuadora.currentTime = 0;
-    }
+function hacerBrrBrr() {
+if( sonidoLicuadora.paused ){
+    botonLicuadora.play();
+    sonidoLicuadora.play();
+  } else {
+    botonLicuadora.play();
+    sonidoLicuadora.pause();
+    sonidoLicuadora.currentTime = 0;
+  }
 } 
