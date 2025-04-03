9ca7554e4c6f6008ead01dde4b8f1fa3f864b6ee
kams <kevinmora@gmail.com>
2025-04-03 10:58:57 -0600

    funcionamiento de la licuadora

---
+++ js/scritps.js
@@ -0,0 +1,5 @@
+
+var estadoLicuadora = "apagado";
+var sonidoLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound");
+var licuadora = document.getElementById("blender");
